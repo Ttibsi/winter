113bab138293f1effe22daaf5c2c88025dc878b8
ttibsi <ashisbitt@icloud.com>
2025-12-12 20:34:49 +0000

    Implement most logic for printing the lexer!

--- src/winter/lexer.cpp
+++ src/winter/lexer.cpp
@@ -8,6 +8,185 @@
 #include "error.h"
 
 namespace Winter {
+    [[nodiscard]] std::string Token::toString() const {
+        switch (type) {
+            case TokenType::LEFT_PAREN:
+                return "TokenType::LEFT_PAREN  "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::RIGHT_PAREN:
+                return "TokenType::RIGHT_PAREN "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::LEFT_BRACE:
+                return "TokenType::LEFT_BRACE  "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::RIGHT_BRACE:
+                return "TokenType::RIGHT_BRACE "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::COMMA:
+                return "TokenType::COMMA       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::DOT:
+                return "TokenType::DOT         "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::SEMICOLON:
+                return "TokenType::SEMICOLON   "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::COLON:
+                return "TokenType::COLON       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::MINUS:
+                return "TokenType::MINUS       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::PLUS:
+                return "TokenType::PLUS        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::STAR:
+                return "TokenType::STAR        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::SLASH:
+                return "TokenType::SLASH       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::BANG:
+                return "TokenType::BANG        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::BANG_EQ:
+                return "TokenType::BANG_EQ     "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::EQUAL:
+                return "TokenType::EQUAL       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::EQUAL_EQ:
+                return "TokenType::EQUAL_EQ    "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::GREATER:
+                return "TokenType::GREATER     "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::GREATER_EQ:
+                return "TokenType::GREATER_EQ  "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::LESS:
+                return "TokenType::LESS        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::LESS_EQ:
+                return "TokenType::LESS_EQ     "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::AND:
+                return "TokenType::AND         "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::OR:
+                return "TokenType::OR          "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::CASE:
+                return "TokenType::CASE        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::CLASS:
+                return "TokenType::CLASS       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::CONST:
+                return "TokenType::CONST       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::DEFAULT:
+                return "TokenType::DEFAULT     "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::ELSE:
+                return "TokenType::ELSE        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::EXPORT:
+                return "TokenType::EXPORT      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::FALSE:
+                return "TokenType::FALSE       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::FOR:
+                return "TokenType::FOR         "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::FUNC:
+                return "TokenType::FUNC        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::IF:
+                return "TokenType::IF          "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::IMPORT:
+                return "TokenType::IMPORT      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::NIL:
+                return "TokenType::NIL         "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::RETURN:
+                return "TokenType::RETURN      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::STATIC:
+                return "TokenType::STATIC      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::SWITCH:
+                return "TokenType::SWITCH      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::TRUE:
+                return "TokenType::TRUE        "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::VAR:
+                return "TokenType::VAR         "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::IDENT:
+                return "TokenType::IDENT       "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::STRING:
+                return "TokenType::STRING      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::NUMBER:
+                return "TokenType::NUMBER      "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+            case TokenType::END:
+                return "TokenType::END         "
+                       "Start: " +
+                       std::to_string(start) + " Len: " + std::to_string(len);
+        }
+
+        return "";
+    }
+
     void Lexer::makeToken(TokenType type, std::size_t start, std::size_t len) {
         tokens.emplace_back(type, start, len);
     }
@@ -92,7 +271,7 @@ namespace Winter {
     }
 
     std::expected<void, Err> Lexer::tokenize() {
-        int advance_count = 0;
+        std::size_t advance_count = 0;
 
         for (const auto&& [idx, c] : std::views::enumerate(raw_text)) {
             if (advance_count) {
