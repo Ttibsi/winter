9f1a7b7767051cc972cf85e49808eddd93fb26b9
ttibsi <ashisbitt@icloud.com>
2026-01-07 22:40:48 +0000

    tests setup

---
+++ tests/bytecode_test.h
@@ -0,0 +1,25 @@
+#include <willow/test.h>
+
+#include "winter/bytecode.h"
+
+constexpr int test_Chunk_extend([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
+constexpr int test_Generator_compileValue([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
+constexpr int test_Generator_compileTok([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
+constexpr int test_Generator_compileExpression([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
+constexpr int test_Generator_compileReturn([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
+constexpr int test_Generator_compileFunc([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
+constexpr int test_Generator_generate([[maybe_unused]] Willow::Test* test) {
+    return 1;
+}
