f6b3ff0563ebd7c82022a29c3a6c467043041746
Ttibsi <ashisbitt@icloud.com>
2026-02-02 08:23:08 +0000

    Clear out old approach

--- src/main.cpp
+++ src/main.cpp
@@ -3,41 +3,36 @@
 #include <string>
 #include <string_view>
 
-#include "winter/winter.h"
+#include "winter.h"
 
 using namespace std::string_view_literals;
 
-void startRepl() {}
+void usage() {}
 
 int main(int argc, char* argv[]) {
-    const auto args = std::span(argv, argc);
-
-    if (args.size() == 1) {
-        startRepl();
-        return 0;
-    }
-
     bool enable_debug;
     std::string filename = "";
+
+    const auto args = std::span(argv, argc);
+    if (args.size() == 1) {
+        usage();
+        return 1;
+    }
 
     for (auto&& elem : args) {
         std::string_view str = std::string_view(elem);
 
         if (str == "-D"sv) { enable_debug = true; }
-        if (str.ends_with(".wt"sv)) { filename = std::string(elem); }
+        if (str.ends_with(".wtx"sv)) { filename = std::string(elem); }
     }
 
     if (filename.empty()) {
         std::println("Error: No file provided");
+        usage();
         return 1;
     }
 
-    auto vm = Winter::VM(enable_debug);
-    Winter::retcode_t ret = vm.doFile(filename);
-    if (!ret.has_value()) {
-        std::println("ERROR: {}", ret.error().msg);
-        return 0;
-    };
+    // TODO: here goes the lexing/parsing etc
 
-    return ret.value();
+    return 0;
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,61 +1,12 @@
 #include <willow/reporters.h>
 #include <willow/willow.h>
 
-#include "bytecode_test.h"
-#include "error_test.h"
-#include "helpers_test.h"
-#include "lexer_test.h"
-#include "object_test.h"
-#include "parser_test.h"
-#include "winter_test.h"
-
 int main() {
     Willow::PreCommitReporter reporter = {};
 
     return Willow::runTests(
         {
-            {"openFile", test_openFile},
-            {"[Object] Constructor", test_objectConstructor},
-            {"[Object] unwrap", test_objectUnwrap},
-            {"[Err] Constructor", test_errConstructor},
-            {"[Err] getErrType", test_getErrType},
-            {"[Err] display", test_display},
-            {"[VM] push", test_vmPush},
-            {"[VM] pop", test_vmPop},
-            {"[VM] doString", test_vmDoString, Willow::Status::Skip},
-            {"[VM] doFile", test_vmDoFile, Willow::Status::Skip},
-            {"[VM] call", test_vmCall, Willow::Status::Skip},
-            {"[Token] toString", test_token_toString},
-            {"[Lexer] makeToken", test_makeToken},
-            {"[Lexer] scanNumber", test_scanNumber},
-            {"[Lexer] scanStringLiteral", test_scanStringLiteral},
-            {"[Lexer] scanChar", test_scanChar},
-            {"[Lexer] validIdentChar", test_validIdentChar},
-            {"[Lexer] scanKeyword", test_scanKeyword},
-            {"[Lexer] scanIdentifier", test_scanIdentifier},
-            {"[Lexer] scanEllipsis", test_scanEllipsis},
-            {"[Lexer] tokenize", test_tokenize},
-            {"[Lexer] advance (TokenType overload)", test_advanceTokOverload},
-            {"[Lexer] advance", test_advance},
-            {"[Lexer] currToken", test_currToken},
-            {"[Lexer] check", test_check},
-            {"[Lexer] checkNext", test_checkNext},
-            {"[Lexer] atEnd", test_atEnd},
-            {"[Parser] prefixBindingPower", test_prefixBindingPower},
-            {"[Parser] infixBindingPower", test_infixBindingPower},
-            {"[Parser] parseBlock", test_parseBlock},
-            {"[Parser] parseExpression", test_parseExpression},
-            {"[Parser] parseFunc", test_parseFunc},
-            {"[Parser] parseReturn", test_parseReturn},
-            {"[Parser] parseStatement", test_parseStatement},
-            {"[Parser] parse_tree", test_parse_tree},
-            {"[Chunk] extend", test_Chunk_extend},
-            {"[Generator] compileValue", test_Generator_compileValue},
-            {"[Generator] compileTok", test_Generator_compileTok},
-            {"[Generator] compileExpression", test_Generator_compileExpression},
-            {"[Generator] compileReturn", test_Generator_compileReturn},
-            {"[Generator] compileFunc", test_Generator_compileFunc},
-            {"[Generator] generate", test_Generator_generate},
+
         },
         reporter);
 }
