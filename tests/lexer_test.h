a0583f1c762a65023b023ee889856f39a0eaf9b7
ttibsi <ashisbitt@icloud.com>
2026-02-08 13:47:18 +0000

    prep for lexer tests

---
+++ tests/lexer_test.h
@@ -0,0 +1,47 @@
+#include <willow/willow.h>
+
+#include "lexer.h"
+
+constexpr auto test_skipWhitespace([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_between([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_isNumeric([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_isLetter([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_lexNumeric([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_lexSingle([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_lexDouble([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_lexChar([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_lexString([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_lexIdentKeyword([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
+
+constexpr auto test_operator_funcCall([[maybe_unused]] Willow::Test* test) -> int {
+    return 1;
+}
