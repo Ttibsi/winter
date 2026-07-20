a9b207fac7e9db1e5ee493b4d8acd1712f2c7717
ttibsi <ashisbitt@icloud.com>
2026-07-20 14:24:48 +0100

    using namespace llvm

--- src/backend/backend.cpp
+++ src/backend/backend.cpp
@@ -13,12 +13,12 @@
 
 #include "../frontend/ast.h"
 
+using namespace llvm;
+
 namespace Winter {
-    [[nodiscard]] std::expected<llvm::Type*, Error> getType(
-        llvm::LLVMContext& ctx,
-        std::string_view type_str) {
+    [[nodiscard]] std::expected<Type*, Error> getType(LLVMContext& ctx, std::string_view type_str) {
         if (type_str == "i32") {
-            llvm::Type* int32Type = llvm::Type::getInt32Ty(ctx);
+            Type* int32Type = Type::getInt32Ty(ctx);
             return int32Type;
         }
 
@@ -27,38 +27,42 @@ namespace Winter {
     }
 
     [[nodiscard]] std::expected<void, Error>
-    createFunction(llvm::LLVMContext& ctx, llvm::Module* mod, Node node, const letNode* let) {
+    createFunction(LLVMContext& ctx, Module* mod, Node node, const letNode* let) {
         const funcNode* func = std::get_if<funcNode>(&node.children.at(0).data);
 
-        std::expected<llvm::Type*, Error> retType = getType(ctx, func->retType);
+        std::expected<Type*, Error> retType = getType(ctx, func->retType);
         if (!retType.has_value()) { return std::unexpected(retType.error()); }
 
-        std::vector<llvm::Type*> paramList = {};
+        std::vector<Type*> paramList = {};
         for (auto param : func->parameters) {
             const paramNode* p = std::get_if<paramNode>(&param.data);
-            std::expected<llvm::Type*, Error> paramType = getType(ctx, p->type);
+            std::expected<Type*, Error> paramType = getType(ctx, p->type);
             if (!paramType.has_value()) { return std::unexpected(paramType.error()); }
             paramList.push_back(paramType.value());
         }
 
-        auto fType = llvm::FunctionType::get(retType.value(), llvm::ArrayRef(paramList), false);
+        auto fType = FunctionType::get(retType.value(), ArrayRef(paramList), false);
         mod->getOrInsertFunction(let->name, fType);
         return {};
     }
 
-    [[nodiscard]] llvm::BasicBlock*
-    createBlock(llvm::LLVMContext& ctx, llvm::Module* mod, Node node, const letNode* let) {
-        const Node func = node.children.at(0);
-        const Node body = func.children.at(0);
-
+    [[nodiscard]] BasicBlock* createBlock(LLVMContext& ctx, Module* mod, const letNode* let) {
         // TODO: populate twine with line number when we have that info
-        auto blk = llvm::BasicBlock::Create(ctx, llvm::Twine(), mod->getFunction(let->name));
+        // NOTE: Twine is like an assembly label, I think
+        auto blk = BasicBlock::Create(ctx, Twine(), mod->getFunction(let->name));
         return blk;
     }
 
+    void populateBlock(LLVMContext& ctx, BasicBlock* blk, const Node node) {
+        const Node func = node.children.at(0);
+        const Node body = func.children.at(0);
+
+        for (auto stmt : body.children) {}
+    }
+
     [[nodiscard]] std::expected<void, Error> compileModule(std::span<Node> nodes) {
-        llvm::LLVMContext ctx;
-        llvm::Module myModule("Main", ctx);
+        LLVMContext ctx;
+        Module myModule("Main", ctx);
 
         for (auto node : nodes) {
             const letNode* let = std::get_if<letNode>(&node.data);
@@ -66,7 +70,11 @@ namespace Winter {
                 std::expected<void, Error> ret = createFunction(ctx, &myModule, node, let);
                 if (!ret.has_value()) { return std::unexpected(ret.error()); }
 
-                llvm::BasicBlock* blk = createBlock(ctx, &myModule, node, let);
+                // NOTE: A function may be made up of multiple basic blocks
+                // probably nested blocks, like if/else/for blocks?
+                BasicBlock* blk = createBlock(ctx, &myModule, let);
+
+                populateBlock(ctx, blk, node);
             }
         }
 
